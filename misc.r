e9980134307385ade099e2e962a769bafe69ddf5
dschluter <schluter@zoology.ubc.ca>
2015-08-19 13:06:03 -0700

    and again

--- misc.r
+++ misc.r
@@ -129,3 +129,32 @@ matchcol<-function(matchto, matchfrom, fromvar){
 		}
 	return(result)
 }
+
+recode <- function(..., ret = c("numeric", "factor"), none = if (ret == "numeric") 0 else "none", na){
+	# stolen from the Hmisc library
+    ret <- match.arg(ret)
+    w <- list(...)
+    if (!is.logical(w[[1]]) && length(w) == 3) {
+        z <- w[[3]][match(w[[1]], w[[2]])]
+        if (!missing(none)) 
+            z[if (is.numeric(none)) 
+                is.na(z)
+            else z == ""] <- none
+        return(z)
+    }
+    nam <- names(w)
+    if (missing(ret)) 
+        ret <- if (all.is.numeric(nam)) 
+            "numeric"
+        else "factor"
+    result <- rep(none, length(w[[1]]))
+    for (i in 1:length(w)) result[w[[i]]] <- if (ret == "numeric") 
+        numnam[i]
+    else nam[i]
+    if (ret == "factor") 
+        result <- as.factor(result)
+    if (!missing(na)) 
+        result[is.na(na)] <- NA
+    result
+}
+

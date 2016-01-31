0a52a52e9395306418a0708edc426e76ad87df37
dschluter <schluter@zoology.ubc.ca>
2016-01-31 12:52:40 -0800

    first changes

--- principalComponents.R
+++ principalComponents.R
@@ -220,11 +220,11 @@ gc()
 
 cat("\nConverting genotypes to 0 1 2 scores (for number of '1' alleles)\n")
 # Count the number of alleles of the type "second allele" (stored in z)
-score <- mapply(genotypes, z, FUN = function(x,z){
+genoScore <- mapply(genotypes, z, FUN = function(x,z){
 	score <- strsplit(x, split = "/")
 	score <- sapply(score, function(score){sum(score == z)})
 	})
-# score[ , 1:3]
+# genoScore[ , 1:3]
 gc()
            # used  (Mb) gc trigger   (Mb)  max used   (Mb)
 # Ncells  5608543 299.6   20248149 1081.4  24336408 1299.8
@@ -235,7 +235,7 @@ rm(genotypes)
 cat("\nPrincipal components analysis\n")
 
 # library(pcaMethods)
-# zscore <- prep(score, scale="none", center=TRUE)
+# zscore <- prep(genoScore, scale="none", center=TRUE)
 # resPCA <- pca(zscore, method="svd", center=FALSE, nPcs=5)
 # gc()
            # used  (Mb) gc trigger   (Mb)  max used   (Mb)
@@ -244,31 +244,34 @@ cat("\nPrincipal components analysis\n")
 
 # sDev(pcaRes)
 
-# *** Drop the Japan Sea fish, as they are outliers ***
-groups <- groupnames[groupcodes]
-score <- score[ groups != "marine-jap", ]
+# -----
+# First, including all fish
 
-z <- prcomp(score, center = TRUE)
+pcaResList <- list() # save results but not genoScore for now
+
+z <- prcomp(genoScore, center = TRUE)
 
 cat("\nProportion of variance accounted for by each PC\n")
-print( round( 100*(z$sdev^2)/sum(z$sdev^2), 2) )
- # [1] 29.54  9.82  5.63  3.96  3.73  2.45  2.13  1.87  1.67  1.58  1.37  1.24
-# [13]  1.18  1.11  1.06  0.98  0.95  0.88  0.85  0.78  0.74  0.72  0.70  0.67
-# [25]  0.65  0.63  0.61  0.60  0.60  0.57  0.56  0.55  0.54  0.52  0.52  0.51
-# [37]  0.50  0.50  0.49  0.48  0.47  0.47  0.46  0.45  0.45  0.45  0.45  0.44
-# [49]  0.44  0.43  0.43  0.42  0.42  0.41  0.41  0.41  0.40  0.40  0.39  0.38
-# [61]  0.38  0.38  0.37  0.37  0.37  0.36  0.36  0.36  0.35  0.35  0.34  0.34
-# [73]  0.33  0.32  0.32  0.31  0.29  0.28  0.27  0.27  0.24  0.00
+pcaVarProp <- 100*(z$sdev^2)/sum(z$sdev^2)
+print( round( pcaVarProp, 2) )
+ # [1] 27.24 17.95  7.38  4.27  3.01  2.83  1.86  1.62  1.42  1.26  1.20  1.04
+# [13]  0.94  0.90  0.84  0.81  0.74  0.72  0.67  0.65  0.59  0.56  0.54  0.53
+# [25]  0.51  0.49  0.48  0.48  0.47  0.46  0.46  0.46  0.45  0.43  0.43  0.42
+# [37]  0.41  0.40  0.39  0.39  0.38  0.38  0.37  0.36  0.36  0.35  0.35  0.34
+# [49]  0.34  0.34  0.34  0.34  0.33  0.33  0.32  0.32  0.32  0.31  0.31  0.31
+# [61]  0.31  0.30  0.30  0.29  0.29  0.29  0.28  0.28  0.28  0.28  0.27  0.27
+# [73]  0.27  0.26  0.26  0.25  0.25  0.25  0.24  0.24  0.22  0.21  0.21  0.21
+# [85]  0.19  0.00
 
-pcaRes <- cbind.data.frame(fishnames = fishnames[groups != "marine-jap"], 
-			groupnames = groups[groups != "marine-jap"], stringsAsFactors = FALSE)
+pcaRes <- cbind.data.frame(fishnames, groupnames[groupcodes], stringsAsFactors = FALSE)
 pcaRes <- cbind.data.frame(pcaRes, z$x)
 
-save(pcaRes, file = pcaResFile) 
-# load(file = pcaResFile) # object is pcaRes
-# load("BenlimAllMarine.chrXXI.pcaRes.rdd") # object is pcaRes
+pcaResList$pcaRes <- pcaRes
+pcaResList$pcaVarProp <- pcaVarProp
 
-# Plots
+# PCA axis plots and phylogram based on distances
+pdf(file = paste(project, chrname, "pcaRes.pdf", sep = "."))
+
 plot(PC2 ~ PC1, data = pcaRes, col = as.numeric(factor(groupnames)), pch = as.numeric(factor(groupnames)) )
 text(PC2 ~ PC1, data = pcaRes, labels = groupnames, pos = 4, offset = 0.5, cex = 0.7, xpd = NA)
 
@@ -284,16 +287,98 @@ text(PC5 ~ PC4, data = pcaRes, labels = groupnames, pos = 4, offset = 0.5, cex =
 # Calculate the distances between all pairs of individuals
 # Euclidean
 x <- z$x
-dimnames(x) <- list(fishnames[groups != "marine-jap"], fishnames[groups != "marine-jap"])
+dimnames(x) <- list(fishnames, fishnames)
 pcaDist <- dist(x, method = "euclidean", diag = TRUE, upper = TRUE)
+
+pcaResList$pcaDist <- pcaDist
 
 # NJ tree using distances
 library(ape)
+
 pcaPhylo <- nj(pcaDist)
-plot(pcaPhylo, cex = 0.3)
-dev.off()
+
+# plot(pcaPhylo, cex = 0.3)
+# plot(pcaPhylo, cex = 0.3, type = "fan")
+plot(pcaPhylo, cex = 0.3, type = "unrooted", lab4ut = "axial")
+# plot(pcaPhylo, cex = 0.3, type = "radial")
+
+# dev.off()
 
 # Improved NJ (?)
-pcaPhylo <- bionj(pcaDist/10) # can't have any values greater than 100
-plot(pcaPhylo, cex = 0.3)
+# pcaPhylo <- bionj(pcaDist/10) # can't have any values greater than 100
+# plot(pcaPhylo, cex = 0.3)
+# dev.off()
+
+
+# -----
+# redo without Sea of Japan
+
+if( "marine-jap" %in% groupnames ){
+	groups <- groupnames[groupcodes]
+	genoScore2 <- genoScore[ groups != "marine-jap", ]
+
+	z <- prcomp(genoScore2, center = TRUE)
+	cat("\nProportion of variance accounted for by each PC\n")
+	pcaVarProp2 <- 100*(z$sdev^2)/sum(z$sdev^2)
+	print( round( pcaVarProp2, 2) )
+	 # [1] 29.54  9.82  5.63  3.96  3.73  2.45  2.13  1.87  1.67  1.58  1.37  1.24
+	# [13]  1.18  1.11  1.06  0.98  0.95  0.88  0.85  0.78  0.74  0.72  0.70  0.67
+	# [25]  0.65  0.63  0.61  0.60  0.60  0.57  0.56  0.55  0.54  0.52  0.52  0.51
+	# [37]  0.50  0.50  0.49  0.48  0.47  0.47  0.46  0.45  0.45  0.45  0.45  0.44
+	# [49]  0.44  0.43  0.43  0.42  0.42  0.41  0.41  0.41  0.40  0.40  0.39  0.38
+	# [61]  0.38  0.38  0.37  0.37  0.37  0.36  0.36  0.36  0.35  0.35  0.34  0.34
+	# [73]  0.33  0.32  0.32  0.31  0.29  0.28  0.27  0.27  0.24  0.00
+	
+	pcaRes2 <- cbind.data.frame(fishnames = fishnames[groups != "marine-jap"], 
+				groupnames = groups[groups != "marine-jap"], stringsAsFactors = FALSE)
+	pcaRes2 <- cbind.data.frame(pcaRes2, z$x)
+	
+	pcaResList$$pcaRes2 <- pcaRes2
+	pcaResList$$pcaVarProp2 <- pcaVarProp2
+
+	# Plots
+	plot(PC2 ~ PC1, data = pcaRes2, col = as.numeric(factor(groupnames)), pch = as.numeric(factor(groupnames)) )
+	text(PC2 ~ PC1, data = pcaRes2, labels = groupnames, pos = 4, offset = 0.5, cex = 0.7, xpd = NA)
+	
+	plot(PC3 ~ PC2, data = pcaRes2, col = as.numeric(factor(groupnames)), pch = as.numeric(factor(groupnames)) )
+	text(PC3 ~ PC2, data = pcaRes2, labels = groupnames, pos = 4, offset = 0.5, cex = 0.7, xpd = NA)
+	
+	plot(PC4 ~ PC3, data = pcaRes2, col = as.numeric(factor(groupnames)), pch = as.numeric(factor(groupnames)) )
+	text(PC4 ~ PC3, data = pcaRes2, labels = groupnames, pos = 4, offset = 0.5, cex = 0.7, xpd = NA)
+	
+	plot(PC5 ~ PC4, data = pcaRes2, col = as.numeric(factor(groupnames)), pch = as.numeric(factor(groupnames)) )
+	text(PC5 ~ PC4, data = pcaRes2, labels = groupnames, pos = 4, offset = 0.5, cex = 0.7, xpd = NA)
+
+
+	# Calculate the distances between all pairs of individuals
+	# Euclidean
+	x <- z$x
+	dimnames(x) <- list(fishnames[groups != "marine-jap"], fishnames[groups != "marine-jap"])
+	pcaDist2 <- dist(x, method = "euclidean", diag = TRUE, upper = TRUE)
+	
+	pcaResList$$pcaDist2 <- pcaDist2
+
+	# NJ tree using distances
+	library(ape)
+	
+	pcaPhylo <- nj(pcaDist2)
+	
+	# plot(pcaPhylo, cex = 0.3)
+	# plot(pcaPhylo, cex = 0.3, type = "fan")
+	plot(pcaPhylo, cex = 0.3, type = "unrooted", lab4ut = "axial")
+	# plot(pcaPhylo, cex = 0.3, type = "radial"])
+	
+	# dev.off()
+	
+	# Improved NJ (?)
+	# pcaPhylo <- bionj(pcaDist/10) # can't have any values greater than 100
+	# plot(pcaPhylo, cex = 0.3)
+	# dev.off()
+
+	}
+
 dev.off()
+
+save(pcaResList, file = pcaResFile) 
+# load(file = pcaResFile) # object is pcaResList
+# load("BenlimAllMarine.chrXXI.pcaRes.rdd") # object is pcaResList

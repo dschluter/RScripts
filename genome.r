aed01bc56e6ed7eb11418cb2b55781b34b92a8e6
dschluter <schluter@zoology.ubc.ca>
2016-07-08 12:14:49 -0700

    Revert "snpAllele moved inside scored=F routine"
    This reverts commit 841bf1b54feb42ccd6d71f1e7c375d30b6e81ab3.

--- genome.r
+++ genome.r
@@ -59,13 +59,7 @@ g$makeSnpgdsFile <- function(geno, pos = NULL, chr = NULL, scored = FALSE, gdsOu
 	# Ensure it is a data frame
 	if(class(geno)!="data.frame") geno <- as.data.frame(geno, stringsAsFactors = FALSE)
 	
-	if(scored){
-		
-		genoscore <- geno
-		snpAllele <- NULL
-		
-		} else{
-		
+	if(scored) genoscore <- geno else{
 		# Identify the unique alleles at every marker (at most 2 are allowed)
 		alleles <- lapply(geno, function(x){
 			genotypes <- unique(x)
@@ -102,10 +96,6 @@ g$makeSnpgdsFile <- function(geno, pos = NULL, chr = NULL, scored = FALSE, gdsOu
 		genoscore <- mapply(geno, alleles, FUN = function(x,y){
 			z2 <- str_count(x, y[2])
 			})
-			
-		# create a variable retaining the original genotypes
-		snpAllele <- sapply(alleles, function(x){paste(x, collapse = "/")})
-
 		}
 
 	# extract the id and marker names (do this after the check for exactly 2 alleles, 
@@ -117,6 +107,9 @@ g$makeSnpgdsFile <- function(geno, pos = NULL, chr = NULL, scored = FALSE, gdsOu
 	# result will be a matrix (required for next step)
 	genoscore <- t(genoscore) 
 	
+	# create a variable retaining the original genotypes
+	snpAllele <- sapply(alleles, function(x){paste(x, collapse = "/")})
+
 	# Put it all into a list
 	genoList <- list(sample.id = id, snp.id = snpname, snp.position = pos, snp.chromosome = chr, 
 				snp.allele = snpAllele, genotype = genoscore)

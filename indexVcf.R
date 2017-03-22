9b93c0ca995d2ce75139019adabfbd390a3325b4
dschluter <schluter@zoology.ubc.ca>
2017-03-21 22:21:02 -0400

    cleaned up file names

--- indexVcf.R
+++ indexVcf.R
@@ -3,14 +3,25 @@
 # To make an index for a .vcf file
 # Here I use VariantAnnotation
 
-# # module load R/3.1.2
-# # R
+# module load R/3.1.2
+# R
 
-args <- commandArgs(TRUE) # first argument will be bamfile name
-# args <- "Benlim.chrM.var.vcf"
+args <- commandArgs(TRUE) # first argument will be vcf.gz name (might not have the gz)
+# args <- "SculpinNoSculpin.var.vcf.gz"
 
 library(VariantAnnotation, quietly = TRUE)
+
+# File names
 vcfname <- args[1]
-compressedVcfname <- paste(vcfname, ".bgz", sep = "")
-zipped <- bgzip(vcfname, compressedVcfname)
-idx <- indexTabix(zipped, "vcf")
+root <- sub("[.]gz$", "", vcfname) # remove the "gz" if present
+compressedVcfname <- paste(root, "bgz", sep = ".") # save to this instead of .gz, which already exists
+
+# compress and save file name
+bgzname <- bgzip(vcfname, compressedVcfname)
+
+# Create the index
+idx <- indexTabix(bgzname, "vcf")
+
+# optional: rename .bgz to .gz (this will overwrite original gz file)
+# file.rename(bgzname, sub("vcf.bgz", "vcf.gz", bgzname))
+# file.rename(idx, sub("vcf.bgz.tbi", "vcf.gz.tbi", idx))
